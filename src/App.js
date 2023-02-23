c0d5beb26269e1c3220156eb9f307af5235fc748
jiyoon <vywns9978@gmail.com>
2023-02-23 16:42:20 +0900

    [feat] 배열을 이용해 List에 아이템(일기) 동적으로 추가하기

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+import { useState, useRef } from "react";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "winter",
-    content: "Hi 1",
-    emotion: 1,
-    create_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "spring",
-    content: "Hi 2",
-    emotion: 3,
-    create_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "fall",
-    content: "Hi 3",
-    emotion: 5,
-    create_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "summer",
-    content: "Hi 4",
-    emotion: 4,
-    create_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "winter",
+//     content: "Hi 1",
+//     emotion: 1,
+//     create_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "spring",
+//     content: "Hi 2",
+//     emotion: 3,
+//     create_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "fall",
+//     content: "Hi 3",
+//     emotion: 5,
+//     create_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: "summer",
+//     content: "Hi 4",
+//     emotion: 4,
+//     create_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const create_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      create_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
